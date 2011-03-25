5a963984173582b779a94f844616aa53b2b61380
Ralph Holzmann <ralphholzmann@gmail.com>
2011-03-25 14:40:56 -0400

    Fixing test email script.

--- test/test-parse.js
+++ test/test-parse.js
@@ -1,6 +1,6 @@
 var fs    = require( 'fs' ),
     util  = require('util'),
-    parse = require( '../parse.js' );
+    parse = require( '../lib/parse.js' );
 
 
 fs.readFile( process.argv[2], 'utf-8', function( err, data ) {
